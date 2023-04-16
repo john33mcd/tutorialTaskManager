d47213ef3ac7be8215168e8fc7f1fa646720e8f7
john33mcd <jmcd-34@hotmail.com>
2023-04-16 17:31:50 +0000

    added form

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Task
+
 
 # inheriting from above import ListView
 
@@ -15,3 +19,11 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = "task"
     template_name = 'base/task_detail.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    # creates model form, takes all fields from model - class Task(
+    # models.Model)
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
