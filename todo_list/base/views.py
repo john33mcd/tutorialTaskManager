47e5b638527335a88e5b18dd1a30ac051c078d07
john33mcd <jmcd-34@hotmail.com>
2023-04-17 16:25:13 +0000

    added delete fnctioality, added login

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -3,12 +3,21 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 from .models import Task
 
 
-# inheriting from above import ListView
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
 
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+# inheriting from above import ListView
 
 class TaskList(ListView):
     model = Task
@@ -32,4 +41,10 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
